8352aa3bafc9db22bbf88c73292112ea020faa2c
Koen Punt <koen@koenpunt.nl>
2013-04-09 00:53:13 +0200

    correctly calculating spares 'n strikes

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,28 +1,47 @@
 class Frame
-  
-  def initialize score_1, score_2
+
+  def initialize score
+    @score = score
     @spare = false
-    @score_1 = score_1
-    @score_2 = score_2
+    @score_1, @score_2 = *score.split('')
   end
-  
-  def score spare = nil
+
+  def score spare = false, strike = false
     score_1 = @score_1
     score_2 = @score_2
 
+    # Strike
+    if score_1 == 'x'
+      score_1 = 10
+      score_2 = 0
+    end
+
+    # Spare
     if score_2 == '/'
       score_2 = 10 - score_1.to_i
     end
 
+    score_1 = score_1.to_i
+    score_2 = score_2.to_i
+
     if spare
-      score_1 = score_1.to_i * 2
+      score_1 = score_1 * 2
     end
-    
-    score_1.to_i + score_2.to_i
+
+    if strike
+      score_1 = score_1 * 2
+      score_2 = score_2 * 2
+    end
+
+    score_1 + score_2
   end
-  
+
   def spare
     @score_2 == '/'
+  end
+
+  def strike
+    @score_1 == 'x'
   end
 
 end
@@ -31,18 +50,21 @@ end
 class Bowling
 
   def initialize score
+    @frames = []
     @score = score
-    @frames = @score.scan(/../).map do |variable|
-      Frame.new(variable[0], variable[1])
+    @score.scan(/[0-9\/-]{2}|x/) do |score|
+      @frames << Frame.new(score)
     end
   end
 
   def score
     spare = false
-    
+    strike = false
+
     @frames.map do |frame|
-      score = frame.score spare
+      score = frame.score spare, strike
       spare = frame.spare
+      strike = frame.strike
       score
     end.reduce(:+)
   end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -4,76 +4,120 @@ require 'bowling'
 
 describe 'Bowling' do
   
-  
   it 'should return 0' do
-    
     bowling = Bowling.new "0000"
-  
     bowling.score.should eq(0)
-    
   end
 
   it 'should return 8' do
     bowling = Bowling.new "3140"
-    
     bowling.score.should eq(8)
   end
-  
+
   it '3 and spare should return 10' do
     bowling = Bowling.new "3/"
-    
     bowling.score.should eq(10)
   end
-  
-  it '3 and gutter should return 3' do
+
+  it 'should not count a miss' do
     bowling = Bowling.new "3-"
-    
     bowling.score.should eq(3)
   end
-  
-  it '3 and 4 and gutter should return 7' do
+
+  it 'should not count misses' do
     bowling = Bowling.new "3--4"
-    
     bowling.score.should eq(7)
   end
-  
-  it '-/30 should duplicate second frame' do
+
+  it 'should duplicate third roll' do
     bowling = Bowling.new "-/30"
     bowling.score.should eq(16)
-  end
-  
-  it '0/03 should duplicate second frame' do
+
     bowling = Bowling.new "0/03"
     bowling.score.should eq(13)
   end
-  
+
+  it 'should validate with a spare as last' do
+    bowling = Bowling.new "133/"
+    bowling.score.should eq(14)
+  end
+
+  it 'should validate with multiple spares' do
+    bowling = Bowling.new "14456/5/91"
+    bowling.score.should eq(58)
+  end
+
+  it 'should validate a strike' do
+    bowling = Bowling.new "14x"
+    bowling.score.should eq(15)
+  end
+
+  it 'should duplicate next 2 rolls after strike' do
+    bowling = Bowling.new "14x30"
+    bowling.score.should eq(21)
+  end
+
+  it 'should run a complete game' do
+    bowling = Bowling.new "14456/5/x017/6/x2/"
+    bowling.score.should eq(127)
+  end
+
 end
 
 describe 'Frame' do
-  
+
   it 'should return 0' do
-    frame = Frame.new('0', '0')
+    frame = Frame.new('00')
     frame.score.should eq(0)
   end
-  
-  it 'should return 10' do
-    frame = Frame.new('1', '9')
+
+  it 'should return 10 (sum of 1 and 9)' do
+    frame = Frame.new('19')
     frame.score.should eq(10)
   end
-  
-  it '3 and spare should return 10' do
-    frame = Frame.new('1', '/')
+
+  it 'should return 10 on spare' do
+    frame = Frame.new('1/')
     frame.score.should eq(10)
   end
-  
-  
-  it 'gutter and spare should return 10' do
-    frame = Frame.new('-', '/')
+
+  it 'should return 10 on spare with -' do
+    frame = Frame.new('-/')
     frame.score.should eq(10)
   end
-  
-  it 'should multiply on spare' do
-    frame = Frame.new('-', '/')
+
+  it 'should identify as spare' do
+    frame = Frame.new('-/')
     frame.spare.should be_true
   end
+
+  it 'should multiply first score' do
+    frame = Frame.new('1/')
+    frame.score(true).should eq(11)
+
+    frame = Frame.new('8/')
+    frame.score(true).should eq(18)
+  end
+
+  it 'should return 10 on strike' do
+    frame = Frame.new('x')
+    frame.score.should eq(10)
+  end
+
+  it 'should identify as strike' do
+    frame = Frame.new('x')
+    frame.strike.should be_true
+  end
+
+  it 'should multiply first and second score' do
+    frame = Frame.new('1/')
+    frame.score(false, true).should eq(20)
+
+    frame = Frame.new('8/')
+    frame.score(false, true).should eq(20)
+
+    frame = Frame.new('12')
+    frame.score(false, true).should eq(6)
+  end
+
 end
